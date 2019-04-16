0976d7dc843b8b58c3a6bd9a1493aaea0d9a16b8
minrui <hustminrui@126.com>
2019-04-16 10:17:00 +0800

    cmp ok

--- install.bash
+++ install.bash
@@ -4,6 +4,8 @@ rm -rf ./server/server
 rm -rf ./server/build
 mkdir -p ./server/build
 cd ./server/build
+
+ln -sf `pwd`/../../../YouCompleteMe/third_party/ycmd/third_party/clang/lib/libclang.so.7 libclang.so
+
 cmake ..
 make
-
